90a223e5d7a8c75fe5d63f33bcd04066b0ffd59b
Pedro Fernandes <pmmr.fernandes@campus.fct.unl.pt>
2022-09-16 10:01:40 +0100

    Initial version of our main file.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Main entrypoint
+ * @author Pedro Fernandes 60694
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("An example github repository.");
+    }
+}
